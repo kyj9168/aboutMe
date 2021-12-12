4ccabd9c27353894dfb5e748a67758e78f22193b
youngjunkim <kyj9168@gmail.com>
2021-12-12 10:46:16 +0900

    스크롤 부드럽게 수정

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 
-import { faPhone, faBirthdayCake, faEnvelope, faStar, faBars, faTimes, faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import {
+    faPhone,
+    faBirthdayCake,
+    faEnvelope,
+    faStar,
+    faBars,
+    faTimes,
+    faArrowRight,
+    faArrowLeft,
+} from '@fortawesome/free-solid-svg-icons';
 
 import Header from './components/Header';
 import Section from './components/Section';
 import Footer from './components/Footer';
 import GlobalStyles from './components/GlobalStyles';
+import smoothscroll from 'smoothscroll-polyfill';
 
+library.add(faPhone, faBirthdayCake, faEnvelope, faStar, faBars, faTimes, faArrowLeft, faArrowRight);
 
-library.add(faPhone, faBirthdayCake, faEnvelope, faStar, faBars, faTimes, faArrowLeft, faArrowRight)
-
-class App extends Component {
-  render() {
+function App() {
+    smoothscroll.polyfill();
     return (
-      <div className="App">
-        <Header />
-        <Section />
-        <Footer />
-        <GlobalStyles />
-      </div>
+        <div className="App">
+            <Header />
+            <Section />
+            <Footer />
+            <GlobalStyles />
+        </div>
     );
-  }
 }
 
 export default App;
