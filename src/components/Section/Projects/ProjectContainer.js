7c02eaf60ba9da88945c519f0e43ea04e898b015
youngjunkim <kyj9168@gmail.com>
2021-12-29 23:19:20 +0900

    프로젝트 스킬 변경

--- src/components/Section/Projects/ProjectContainer.js
+++ src/components/Section/Projects/ProjectContainer.js
@@ -1,100 +1,89 @@
-import React, { Component } from "react";
-import ProjectPresenter from "./ProjectPresenter";
-import CIMG from "assets/project/mobileqi.png";
-import PIMG from "assets/project/myproject.png";
-import CIMG2 from "assets/project/mobileqi2.png";
-import ISSUEIMG1 from "assets/project/issueChat1.png";
-import ISSUEIMG2 from "assets/project/issueChat2.png";
-import RCMSIMG1 from "assets/project/rcms1.png";
-import HEALIMG from "assets/project/healing.png";
-import ABOUTPET1 from "assets/project/aboutpet1.png";
-import ABOUTPET2 from "assets/project/aboutpet2.png";
-import QIT from "assets/project/qit.png";
+import React, { Component } from 'react';
+import ProjectPresenter from './ProjectPresenter';
+import CIMG from 'assets/project/mobileqi.png';
+import PIMG from 'assets/project/myproject.png';
+import CIMG2 from 'assets/project/mobileqi2.png';
+import ISSUEIMG1 from 'assets/project/issueChat1.png';
+import ISSUEIMG2 from 'assets/project/issueChat2.png';
+import RCMSIMG1 from 'assets/project/rcms1.png';
+import HEALIMG from 'assets/project/healing.png';
+import ABOUTPET1 from 'assets/project/aboutpet1.png';
+import ABOUTPET2 from 'assets/project/aboutpet2.png';
+import QIT from 'assets/project/qit.png';
 
 class ProjectContainer extends Component {
     state = {
         data: [
             {
-                id: "문화정보원 큐아이 챗봇API, 모바일큐아이 2차 개발",
-                url: "https://qi.kcisa.kr/",
-                txt: [
-                    "문화정보원 기관의 국립중앙도서관, 국립어린이도서관, 제주국제공항, 국립제주박물관 4개 기관의 챗봇 API 구축과 모바일 큐아이 챗봇 화면 개발",
-                ],
-                period: "2019.11 ~ 2019.12",
-                per: "첫 node.js 프로젝트로 node.js를 활용하여 챗봇 API를 구축하고 ejs로 챗봇의 화면 개발을 진행했습니다.",
-                img: [CIMG, CIMG2],
-                skill: ["Node.js", "ElasticSearch", "ejs", "공공데이터포털"],
-            },
-            {
-                id: " 산업기술평가원 RCMS 챗봇 개발",
-                txt: [
-                    "비목추천 API를 활용한 챗봇 API 개발과 react.js를 사용한 챗봇 화면 개발",
-                ],
-                period: "2019.12 ~ 2020.6",
-                per: "ejs로만 개발하면 챗봇 화면의 프론트단을 react.js를 도입하자 제안하여 이 프로젝트 이후로 모든 챗봇 프로젝트의 master 챗봇 템플릿을 개발하였습니다.",
-                img: [RCMSIMG1],
-                skill: ["Node.js", "ElasticSearch", "react.js"],
-            },
-            {
-                id: "문화정보원 큐아이 챗봇API, 모바일큐아이 3차 개발",
-                url: "https://qi.kcisa.kr/",
-                txt: [
-                    "문화정보원 기관의 국립태권도박물관, 국립아시아문화전당, 국립국악원 3개 기관의 챗봇 API 구축과 모바일 큐아이 챗봇 화면 개발",
-                ],
-                period: "2020.06 ~ 2021.01",
-                per: "챗봇 프론트 단의 모든 오류와 챗봇 기존에 없던 자동완성 API, 금지어 API, 미응답 질의 자동추천 API를 직접 개발하여 회사 내 서비스로 배포하였습니다",
-                img: [CIMG, CIMG2],
-                skill: ["Node.js", "ElasticSearch", "ejs", "공공데이터포털"],
-            },
-            {
-                id: "산업기술평가원 전신건강 플랫폼 챗봇 개발",
-                txt: [
-                    "react.js를 활용한 웹앱 챗봇 화면 개발 및 스트레스 지수 파악 API 개발",
-                ],
-                period: "2020.06 ~ 2020.12",
-                per: "회사내에 첫 웹앱 프로젝트로써 앱 개발을 하는 회사와 소통하며 react.js로 챗봇의 프론트 단을 개발하여 웹앱을 출시하였습니다.",
-                img: [HEALIMG],
-                skill: ["Node.js", "ElasticSearch", "ejs"],
-            },
-            {
-                id: "GSretail aboutPet 검색 및 추천 API 개발",
-                url: "https://www.aboutpet.co.kr/shop/home/",
-                txt: [
-                    "태그방식을 활용하여 자카드 유사도를 이용한 추천API 개발",
-                ],
-                period: "2020.06 ~ 2020.12",
-                per: "회사의 검색솔루션을 하나의 API로 통합하기 위한 standard API를 개발하였고 태그 방식의 추천 로직을 직접 구성하여 추천 API 개발을 하였습니다.",
-                img: [ABOUTPET1, ABOUTPET2],
-                skill: ["Node.js", "ElasticSearch", "Kafka", "Nifi"],
-            },
-            {
-                id: "사내 개인프로젝트: QIT (Quality Improvement Test)",
-                txt: ["챗봇 지식구축 작가들을 위한 테스트 툴 개발"],
-                period: "2020.06 ~ 2021.09",
-                per: "사내 작가들을 위해 챗봇 지식을 구성하고 QIT를 돌려 각각 지식이 제대로 구성이 되어있는지 테스트 할 수있는 툴을 개발하였습니다. 이로인해 챗봇의 응답률을 더욱 높일 수 있게 되어 챗봇 솔루션에 보탬이 되었습니다.",
-                img: [QIT],
-                skill: ["Node.js"],
-            },
-            {
-                id: "Y/Project (개인 프로젝트)",
-                git: "https://github.com/kyj9168",
-                url: "http://kyj9168.ddns.net:6100/",
-                txt: [
-                    "React.js를 활용하여 각 메뉴마다 따로 프로세스를 구성하여 만든 개인 프로젝트",
-                ],
+                id: 'Y/Project (개인 프로젝트)',
+                git: 'https://github.com/kyj9168',
+                url: 'http://kyj9168.ddns.net:6100/',
+                txt: ['React.js를 활용하여 각 메뉴마다 따로 프로세스를 구성하여 만든 개인 프로젝트'],
                 img: [PIMG],
-                skill: ["Node.js", "React.js"],
+                skill: ['Node.js', 'React.js'],
             },
             {
-                id: "[개발중]Issue Chat (개인 프로젝트)",
-                git: "https://github.com/kyj9168",
-                url: "http://kyj9168.ddns.net:6100/chat",
+                id: '[개발중]Issue Chat (개인 프로젝트)',
+                git: 'https://github.com/kyj9168',
+                url: 'http://kyj9168.ddns.net:6100/chat',
                 txt: [
-                    "socket.io를 활용하여 채팅의 대화를 kafka를 통해 elasticsearch에 색인한다.",
-                    "nifi를 통해 batch프로세스를 개발하여 elasticsearch nori 형태소 분석기를 이용하여 키워드만 뽑아 실시간 대화의 주요 키워드를 랭킹으로 표현해준다.",
+                    'socket.io를 활용하여 채팅의 대화를 kafka를 통해 elasticsearch에 색인한다.',
+                    'nifi를 통해 batch프로세스를 개발하여 elasticsearch nori 형태소 분석기를 이용하여 키워드만 뽑아 실시간 대화의 주요 키워드를 랭킹으로 표현해준다.',
                 ],
                 img: [ISSUEIMG1, ISSUEIMG2],
-                skill: ["Node.js", "socket.io","Kafka","Nifi"],
+                skill: ['Node.js', 'socket.io', 'Kafka', 'Nifi'],
+            },
+            {
+                id: '문화정보원 큐아이 챗봇API, 모바일큐아이 2차 개발',
+                url: 'https://qi.kcisa.kr/',
+                txt: ['문화정보원 기관의 국립중앙도서관, 국립어린이도서관, 제주국제공항, 국립제주박물관 4개 기관의 챗봇 API 구축과 모바일 큐아이 챗봇 화면 개발'],
+                period: '2019.11 ~ 2019.12',
+                per: '첫 node.js 프로젝트로 node.js를 활용하여 챗봇 API를 구축하고 ejs로 챗봇의 화면 개발을 진행했습니다.',
+                img: [CIMG, CIMG2],
+                skill: ['Node.js', 'ElasticSearch', 'ejs', '공공데이터포털'],
+            },
+            {
+                id: ' 산업기술평가원 RCMS 챗봇 개발',
+                url:'https://chatbot.rcms.go.kr/',
+                txt: ['비목추천 API를 활용한 챗봇 API 개발과 react.js를 사용한 챗봇 화면 개발'],
+                period: '2019.12 ~ 2020.6',
+                per: 'ejs로만 개발하면 챗봇 화면의 프론트단을 react.js를 도입하자 제안하여 이 프로젝트 이후로 모든 챗봇 프로젝트의 master 챗봇 템플릿을 개발하였습니다.',
+                img: [RCMSIMG1],
+                skill: ['Node.js', 'ElasticSearch', 'react.js'],
+            },
+            {
+                id: '문화정보원 큐아이 챗봇API, 모바일큐아이 3차 개발',
+                url: 'https://qi.kcisa.kr/',
+                txt: ['문화정보원 기관의 국립태권도박물관, 국립아시아문화전당, 국립국악원 3개 기관의 챗봇 API 구축과 모바일 큐아이 챗봇 화면 개발'],
+                period: '2020.06 ~ 2021.01',
+                per: '챗봇 프론트 단의 모든 오류와 챗봇 기존에 없던 자동완성 API, 금지어 API, 미응답 질의 자동추천 API를 직접 개발하여 회사 내 서비스로 배포하였습니다',
+                img: [CIMG, CIMG2],
+                skill: ['Node.js', 'ElasticSearch', 'ejs', '공공데이터포털'],
+            },
+            {
+                id: '산업기술평가원 전신건강 플랫폼 챗봇 개발',
+                txt: ['react.js를 활용한 웹앱 챗봇 화면 개발 및 스트레스 지수 파악 API 개발'],
+                period: '2020.06 ~ 2020.12',
+                per: '회사내에 첫 웹앱 프로젝트로써 앱 개발을 하는 회사와 소통하며 react.js로 챗봇의 프론트 단을 개발하여 웹앱을 출시하였습니다.',
+                img: [HEALIMG],
+                skill: ['Node.js', 'ElasticSearch', 'ejs'],
+            },
+            {
+                id: 'GSretail aboutPet 검색 및 추천 API 개발',
+                url: 'https://www.aboutpet.co.kr/shop/home/',
+                txt: ['태그방식을 활용하여 자카드 유사도를 이용한 추천API 개발'],
+                period: '2020.06 ~ 2020.12',
+                per: '회사의 검색솔루션을 하나의 API로 통합하기 위한 standard API를 개발하였고 태그 방식의 추천 로직을 직접 구성하여 추천 API 개발을 하였습니다.',
+                img: [ABOUTPET1, ABOUTPET2],
+                skill: ['Node.js', 'ElasticSearch', 'Kafka', 'Nifi'],
+            },
+            {
+                id: '사내 개인프로젝트: QIT (Quality Improvement Test)',
+                txt: ['챗봇 지식구축 작가들을 위한 테스트 툴 개발'],
+                period: '2020.06 ~ 2021.09',
+                per: '사내 작가들을 위해 챗봇 지식을 구성하고 QIT를 돌려 각각 지식이 제대로 구성이 되어있는지 테스트 할 수있는 툴을 개발하였습니다. 이로인해 챗봇의 응답률을 더욱 높일 수 있게 되어 챗봇 솔루션에 보탬이 되었습니다.',
+                img: [QIT],
+                skill: ['Node.js'],
             },
         ],
     };
--- src/components/Section/Skill/SkillContainer.js
+++ src/components/Section/Skill/SkillContainer.js
@@ -1,145 +1,121 @@
-import React, { Component } from "react";
-import SkillPresenter from "./SkillPresenter";
-import HTMLIMG from "assets/skill/html5.png";
-import CSSIMG from "assets/skill/css3.png";
-import JSIMG from "assets/skill/javascript.png";
-import JQIMG from "assets/skill/jquery.png";
-import REACTIMG from "assets/skill/react.png";
-import NODEIMG from "assets/skill/nodejs.png";
-import GITIMG from "assets/skill/git.png";
-import JAVAIMG from "assets/skill/java.png";
-import JPAIMG from "assets/skill/jpa.png";
-import KAFKAMG from "assets/skill/kafka.jpg";
-import REDISIMG from "assets/skill/redis.png";
-import MYSQLIMG from "assets/skill/mysql.png";
-import SPBTIMG from "assets/skill/springboot.png";
-import AWSIMG from "assets/skill/aws.png";
-import LINUXIMG from "assets/skill/linux.png";
-import JSPIMG from "assets/skill/jsp.png";
-import bitbucketIMG from "assets/skill/bitbucket.jpg";
-import ELKIMG from "assets/skill/elk.png";
-import NIFIIMG from "assets/skill/nifi.png";
+import React, { Component } from 'react';
+import SkillPresenter from './SkillPresenter';
+import HTMLIMG from 'assets/skill/html5.png';
+import CSSIMG from 'assets/skill/css3.png';
+import JSIMG from 'assets/skill/javascript.png';
+import JQIMG from 'assets/skill/jquery.png';
+import REACTIMG from 'assets/skill/react.png';
+import NODEIMG from 'assets/skill/nodejs.png';
+import GITIMG from 'assets/skill/git.png';
+import JAVAIMG from 'assets/skill/java.png';
+import JPAIMG from 'assets/skill/jpa.png';
+import KAFKAMG from 'assets/skill/kafka.jpg';
+import REDISIMG from 'assets/skill/redis.png';
+import MYSQLIMG from 'assets/skill/mysql.png';
+import SPBTIMG from 'assets/skill/springboot.png';
+import AWSIMG from 'assets/skill/aws.png';
+import LINUXIMG from 'assets/skill/linux.png';
+import JSPIMG from 'assets/skill/jsp.png';
+import bitbucketIMG from 'assets/skill/bitbucket.jpg';
+import ELKIMG from 'assets/skill/elk.png';
+import NIFIIMG from 'assets/skill/nifi.png';
 
 class SkillContainer extends Component {
     state = {
         data: [
             {
-                skill: "HTML5",
-                img: HTMLIMG,
-                lv: 4,
-                txt: "프론트엔드의 기본, 웹표준과 HTML태그에 대해 학습하였습니다.",
-            },
-            {
-                skill: "CSS3",
-                img: CSSIMG,
-                lv: 3,
-                txt: "웹페이지의 스타일링과 반응형 레이아웃을 학습하였습니다.",
-            },
-            {
-                skill: "JavaScript",
+                skill: 'JavaScript',
                 img: JSIMG,
                 lv: 5,
-                txt: "가장 좋아하는 언어입니다. ES6문법을 사용 할 수 있습니다.",
+                txt: '가장 좋아하는 언어입니다. ES6문법 이상을 사용 할 수 있습니다.',
             },
             {
-                skill: "Jquery",
+                skill: 'Jquery',
                 img: JQIMG,
-                lv: 4,
-                txt: "자바스크립트로 개발한 코드를 Jquery로 전환하는걸 어려워하지 않습니다",
+                lv: 5,
+                txt: '자바스크립트로 개발한 코드를 Jquery로 전환하는걸 어려워하지 않습니다',
             },
             {
-                skill: "Node.js",
+                skill: 'Node.js',
                 img: NODEIMG,
                 lv: 5,
-                txt: "개발자로 취업후 약 1년반동안 모든 프로젝트의 백엔드를 node.js로 개발해 왔습니다.",
+                txt: '개발자로 취업후 모든 프로젝트의 백엔드를 node.js로 개발해 왔습니다.',
             },
             {
-                skill: "React.js",
+                skill: 'React.js',
                 img: REACTIMG,
-                lv: 2,
-                txt: "프론트쪽으로 가장 많이 진행한 라이브러리이며 현재도 이 라이브러리를 더 잘 사용하기 위해 공부중에 있습니다.",
+                lv: 4,
+                txt: '프론트쪽으로 가장 많이 진행한 라이브러리이며 redux에서는 saga 미들웨어를 사용했고 hook 또한 사용 가능합니다.',
             },
 
             {
-                skill: "KAFKA",
+                skill: 'KAFKA',
                 img: KAFKAMG,
-                lv: 1,
-                txt: "Nifi에 kafka를 연동하여 많은 처리량을 부하없이 처리하도록 사용하였습니다.",
-            },
-            {
-                skill: "NIFI",
-                img: NIFIIMG,
                 lv: 2,
-                txt: "RDB의 데이터를 ElasticSearch로 수집 하여 데이터를 다룰수 있고 실시간 연동을 위해 CDC를 공부하고 있습니다.",
+                txt: 'Nifi에 kafka를 연동할 수 있고 서버3대로 클러스터를 구성해본 경험이 있습니다.',
+            },
+            {
+                skill: 'NIFI',
+                img: NIFIIMG,
+                lv: 3,
+                txt: 'RDB의 데이터를 ElasticSearch로 수집 하고 데이터를 정재하는 작업이 가능합니다.',
             },
 
             {
-                skill: "REDIS",
-                img: REDISIMG,
-                lv: 1,
-                txt: "session관리를 위해 사용해보았습니다.",
-            },
-
-            {
-                skill: "ELK STACK",
+                skill: 'ELK STACK',
                 img: ELKIMG,
                 lv: 3,
-                txt: "형태소 분석기를 이용한 프로젝트 경험이 많아 ELK STACK을 잘 다룰수 있습니다.",
+                txt: '형태소 분석기를 이용한 프로젝트 경험이 많아 ELK STACK을 잘 다룰수 있습니다.',
             },
             {
-                skill: "JAVA",
-                img: JAVAIMG,
-                lv: 2,
-                txt: "교육원에서 배웠던 언어로 현재 서브 언어로 공부중에 있습니다.",
-            },
-            {
-                skill: "JSP",
-                img: JSPIMG,
-                lv: 2,
-                txt: "spring을 공부할때 가장 많이 사용한 프론트단 언어입니다.",
-            },
-            {
-                skill: "JPA",
-                img: JPAIMG,
+                skill: 'AWS',
+                img: AWSIMG,
                 lv: 1,
-                txt: "SpringBoot를 공부할때 CRUD를 JPA로 개발하여 Rest API를 개발해본 경험이 있습니다.",
+                txt: 'ec2, rds 설치가 가능합니다. 직접 서버를 만들고 운영까지 경험해 보았습니다',
+            },
+            {
+                skill: 'LINUX',
+                img: LINUXIMG,
+                lv: 3,
+                txt: '프로세스를 설치 배포 모두 가능하고 bash 파일 작성, 환경변수, 계정설정 모두 가능합니다. 현재 개인서버 또한 Linux CentOS입니다.',
             },
 
             {
-                skill: "Spring Boot",
+                skill: 'Git',
+                img: GITIMG,
+                lv: 3,
+                txt: 'Git의 기본 명령어를 학습하고 현재 활용하고 있습니다.',
+            },
+            {
+                skill: 'JAVA',
+                img: JAVAIMG,
+                lv: 2,
+                txt: '교육원에서 배웠던 언어로 현재 서브 언어로 공부중에 있습니다.',
+            },
+            {
+                skill: 'JSP',
+                img: JSPIMG,
+                lv: 2,
+                txt: 'spring을 공부할때 가장 많이 사용한 프론트단 언어입니다.',
+            },
+            {
+                skill: 'JPA',
+                img: JPAIMG,
+                lv: 1,
+                txt: 'SpringBoot를 공부할때 CRUD를 JPA로 개발하여 Rest API를 개발해본 경험이 있습니다.',
+            },
+
+            {
+                skill: 'Spring Boot',
                 img: SPBTIMG,
                 lv: 1,
-                txt: "회사 업무 외로 공부하여 Rest API를 개발하고 게시판을 만들어 본적이 있습니다.",
+                txt: '회사 업무 외로 공부하여 Rest API를 개발하고 게시판을 만들어 본적이 있습니다.',
             },
             {
-                skill: "MY-SQL",
+                skill: 'MY-SQL',
                 img: MYSQLIMG,
                 lv: 1,
-                txt: "CRUD와 테이블 생성까지 구성하여 쿼리문을 작성 할 수 있습니다.",
-            },
-            {
-                skill: "AWS",
-                img: AWSIMG,
-                lv: 1,
-                txt: "ec2, rds 설치가 가능합니다. 직접 서버를 만들고 운영까지 경험해 보았습니다",
-            },
-            {
-                skill: "LINUX",
-                img: LINUXIMG,
-                lv: 2,
-                txt: "프로세스를 설치 배포 모두 가능하고 bash 파일 작성, 환경변수, 계정설정 모두 가능합니다.",
-            },
-            {
-                skill: "bitbucket",
-                img: bitbucketIMG,
-                lv: 2,
-                txt: "Git의 기본 명령어를 학습하고 현재 활용하고 있습니다.",
-            },
-            {
-                skill: "Git",
-                img: GITIMG,
-                lv: 2,
-                txt: "Git의 기본 명령어를 학습하고 현재 활용하고 있습니다.",
+                txt: 'CRUD와 테이블 생성까지 구성하여 쿼리문을 작성 할 수 있습니다.',
             },
         ],
     };
