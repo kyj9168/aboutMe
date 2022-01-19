c378b946455721f1bc7f7beaa3946e7c0c1dbbd3
youngjunkim <kyj9168@gmail.com>
2022-01-20 00:39:04 +0900

    footer 에 아이콘 추가

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,8 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import NOTION from '../assets/notion_logo.png';
 
 const Footer = styled.footer`
     text-align: center;
@@ -10,15 +13,76 @@ const Footer = styled.footer`
 `;
 
 const Copyright = styled.p`
-    padding: 20px;
+    padding: 0 20px 20px 20px;
     font-size: 18px;
     color: #999;
 `;
 
+const NotionIcon = styled.img`
+    width: 100%;
+    height: 100%;
+    border-radius: 20%;
+    display: block;
+    opacity: 0.6;
+    transition: opacity 0.2s ease-in-out;
+    &:hover {
+        opacity: 1;
+    }
+`;
+
+const ItemLink = styled.a`
+    transition: color 0.2s ease-in-out;
+    word-break: break-all;
+    margin: 25px 15px 15px 15px;
+    height: 30px;
+    display: inline-block;
+    width: 30px;
+    cursor: pointer;
+    &:hover {
+        color: #fff;
+    }
+`;
+
 export default () => (
     <Footer>
+        <ItemLink target="_blank" href="tel:010-9001-6800">
+            <FontAwesomeIcon
+                style={{
+                    display: 'block',
+                    width: '100%',
+                    height: '100%',
+                }}
+                icon="phone"
+                size="2x"
+            />
+        </ItemLink>
+        <ItemLink target="_blank" href="mailto:kyj9168@naver.com">
+            <FontAwesomeIcon
+                style={{
+                    display: 'block',
+                    width: '100%',
+                    height: '100%',
+                }}
+                icon="envelope"
+                size="2x"
+            />
+        </ItemLink>
+        <ItemLink target="_blank" href="https://github.com/kyj9168">
+            <FontAwesomeIcon
+                style={{
+                    display: 'block',
+                    width: '100%',
+                    height: '100%',
+                }}
+                icon={faGithub}
+                size="2x"
+            />
+        </ItemLink>
+        <ItemLink target="_blank" href="https://kyjportfolio.notion.site/kyjportfolio/2382908379ac4bc5b44087e187d6e00e">
+            <NotionIcon src={NOTION} size="2x" />
+        </ItemLink>
         <Copyright>
-            © 2022 Copyright <span style={{ color: "#ddd" }}>YoungJun</span>
+            © 2022 Copyright <span style={{ color: '#ddd' }}>YoungJun</span>
         </Copyright>
     </Footer>
 );
